8dfa929a4250d51be12bb9e83761e0a2b60e5377
shannonZhong <zhongshiran@gmaili.com>
2024-07-19 19:22:30 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,28 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from "./user-input/user-input.component";  
 import type { InvestmentInput } from '../investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 
 })
 export class AppComponent {
+
+  resultsData?:{
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+
+
+  }[];
+
   onCalculateInvestmentResults(data:InvestmentInput) {
 
     const {initialInvestment,duration,expectedReturn,annualInvestment} =data;
@@ -33,7 +46,7 @@ export class AppComponent {
       });
     }
   
-    console.log(annualData);
+    this.resultsData = annualData;
   }
   
 }
